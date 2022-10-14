b566e4c5ed91b2243e77906be7b74b7b659eae7b
Pedro Vidal <pedrovidal2703@gmail.com>
2022-10-14 00:34:36 -0300

    :sparkles: Added header that responds to div in viewport

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener, ElementRef} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -13,23 +13,52 @@ export class HeaderComponent implements OnInit {
     {"pillName": "Stacks", "pillId": "stacks"},
     {"pillName": "Formação", "pillId": "scholarity"}
   ];
-  activePage: string = "Sobre Mim";
+  activePage: string = "about";
+  activePageIndex: number = 1;
+  scrollDebounce: boolean = true;
+  box: any = document.querySelector('#about');
+
+  isInViewport(element: any) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
   clickedPill(pill: any){
-    this.activePage = pill.pillName;
-    console.log(`scrolling to ${pill.pillId}`);
+    this.activePage = pill.pillId;
+    this.activePageIndex = pill.indexOf;
     let el = document.getElementById(pill.pillId);
-    console.log(document.getElementById(pill.pillId))
-    // Provavel função assincrona, transformar depois e ver o resultado
-    setTimeout(function () {
       if(el != null) {
-        el.scrollIntoView({
-          behavior: "smooth",
-          block: "start",
-      });
+        el.scrollIntoView(true);
       }
- }, 100);
   }
+
+  @HostListener('window:scroll', ['$event'])
+  // Temporary solution, it may be CPU intensive
+  onScroll(event: any) {
+    if(this.scrollDebounce) {
+      this.scrollDebounce = false;
+      this.box = document.querySelector('#' + this.activePage);
+      var result = this.isInViewport(this.box);
+
+      if(!result) {
+        this.pills.forEach(
+          (pill) => {
+            var isInView = this.isInViewport(document.querySelector('#' + pill.pillId));
+            if(isInView) {
+              this.activePage = pill.pillId;
+            }
+          }
+        )
+      }
+      setTimeout(() => {
+        this.scrollDebounce = true;
+      }, 10)
+    }
+  };
 
   constructor() { }
 
