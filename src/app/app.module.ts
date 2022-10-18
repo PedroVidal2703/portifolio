14009215a0706f02b9e73696bb3c4b47c56f72a7
Pedro Vidal <pedrovidal2703@gmail.com>
2022-10-18 15:28:52 -0300

    :sparkles: icons added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { SobremimComponent } from './component/sobremim/sobremim.component';
 import { ProjetosComponent } from './component/projetos/projetos.component';
+import { StacksComponent } from './component/stacks/stacks.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     SobremimComponent,
-    ProjetosComponent
+    ProjetosComponent,
+    StacksComponent
   ],
   imports: [
     BrowserModule,
